0bd4511c9224f8cb9d526deee3a4101ab3e0eb7f
David Kowis <dkowis@shlrm.org>
2013-03-04 16:55:59 -0600

    Load the jars from the right path

--- config/boot.rb
+++ config/boot.rb
@@ -5,6 +5,6 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # load up the java jars
 require 'java'
-Dir["#{File.expand_path("../lib/jars", __FILE__)}/*.jar"].each do |jar|
-    require jar
+Dir["#{File.expand_path("../../lib/jars", __FILE__)}/*.jar"].each do |jar|
+  require jar
 end
