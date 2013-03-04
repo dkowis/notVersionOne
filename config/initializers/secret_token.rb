a263be0ab4cf81c0d2b2e1c1a1f7f36bf5343cf2
David Kowis <dkowis@shlrm.org>
2013-03-04 14:21:41 -0600

    Initial import of new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NotVersionOne::Application.config.secret_key_base = '5c8d8fd52cd836f19445ae0e55e0669aa6bd23ee48b52ad201dafbbbcec3486de2e5060a6461423a02768fb7aa7dec0894c5347f5f3ea9d55c6dccf6881895f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NotVersionOne::Application.config.session_store :encrypted_cookie_store, key: '_notVersionOne_session'
